730d14eb1e5af078779756e08ea1e8b0ca830e6b
Ana <anaadeladelrosario@gmail.com>
2025-01-15 21:37:31 +0100

    updating text

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astrofy | Personal Portfolio Website Template";
+export const SITE_TITLE = "CV Portfolio Website Ana";
 export const SITE_DESCRIPTION =
-  "Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.";
+  "Personal Portfolio Website built with Astro and TailwindCSS.";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
 
@@ -126,3 +126,7 @@ export const Skills: Array<SkillItem> = [
   { Item: "Linux OS (basics)" },
   { Item: "AzureDevOps" },
 ];
+
+// setup in astro.config.mjs
+const BASE_URL = new URL(import.meta.env.SITE);
+export const SITE_URL = BASE_URL.origin;
